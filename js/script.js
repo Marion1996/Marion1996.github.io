493d00826644240146f251f942fba692d2d93e66
Marion1996 <Marion1996@users.noreply.github.com>
2017-04-29 14:39:52 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,10 +17,16 @@ $(document).ready(function() {
 
 		success : function(data) {
 			$('#bitcoin_block_number').append(data.blocks);
+			$('#bitcoin_block_number').append(data.height);
+      			$('#bitcoin_mining_number').append(data.time);
+      			$('#bitcoin_peer_number').append(data.peer_count);
 		},
 
 		error : function(xhr, status, err) {
 			$('#bitcoin_block_number').append(err+" N/A");
+			$('#bitcoin_block_number').append(err+" N/A");
+      			$('#bitcoin_mining_number').append(err+" N/A");
+      			$('#bitcoin_peer_number').append(err+" N/A");
 		}
 	});
 });
