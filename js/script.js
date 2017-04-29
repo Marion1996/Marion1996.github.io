1ee0ebe3573319a518843e3a5614b962c9a96863
Marion1996 <Marion1996@users.noreply.github.com>
2017-04-29 15:22:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,9 +6,33 @@
  * 
  */
 
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("demo").innerHTML = jsonPretty;
+    }
+};
+xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.send();
+
+
 function homePageLoading() {
-    url = 'apiUrl';
-    elementID = 'idDeVotreElementHtml';
+    url = 'https://bitcoin.mubiz.com/info';
+    elementID = 'bitcoin_info_number';
+    fonctionRequeteApi(url, elementID);
+	
+    url = 'https://bitcoin.mubiz.com/blockchaininfo';
+    elementID = 'bitcoin_block_number';
+    fonctionRequeteApi(url, elementID);
+	
+    url = 'https://bitcoin.mubiz.com/mininginfo';
+    elementID = 'bitcoin_mining_number';
+    fonctionRequeteApi(url, elementID);
+	
+    url = 'https://bitcoin.mubiz.com/peerinfo;
+    elementID = 'bitcoin_peer_number';
     fonctionRequeteApi(url, elementID);
 }
 
